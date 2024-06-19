a39158ecff465271e9cfce7b6c03b0dd3e1302e5
gursewakinsan <insagursewakinsa@gmail.com>
2024-06-19 16:53:52 +0530

    fixs

--- Controls/CustomBusyIndicator.cs
+++ Controls/CustomBusyIndicator.cs
@@ -1,21 +1,30 @@
-﻿using Syncfusion.Maui.Core;
-
-namespace WorqApp.Controls
+﻿namespace WorqApp.Controls
 {
-    public class CustomBusyIndicator : SfBusyIndicator
+    /* public class CustomBusyIndicator : SfBusyIndicator
+     {
+         public CustomBusyIndicator()
+         {
+             Title = "Loading...";
+             AnimationType = AnimationType.DoubleCircle;
+             DurationFactor = 0.5;
+             FontSize = 16;
+             IndicatorColor = Color.FromArgb("#FFFFFF");
+             SizeFactor = 0.5;
+             TextColor = Color.FromArgb("#FFFFFF");
+             TitleSpacing = 20;
+             HorizontalOptions = LayoutOptions.Center;
+             VerticalOptions = LayoutOptions.Center;
+         }
+     }*/
+
+    public class CustomBusyIndicator : ActivityIndicator
     {
         public CustomBusyIndicator()
         {
-            Title = "Loading...";
-            AnimationType = AnimationType.DoubleCircle;
-            DurationFactor = 0.5;
-            FontSize = 16;
-            IndicatorColor = Color.FromArgb("#FFFFFF");
-            SizeFactor = 0.5;
-            TextColor = Color.FromArgb("#FFFFFF");
-            TitleSpacing = 20;
             HorizontalOptions = LayoutOptions.Center;
             VerticalOptions = LayoutOptions.Center;
         }
+        public Color IndicatorColor { get; set; }
+        public Color TextColor { get; set; }
     }
 }
