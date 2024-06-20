b956e3fc7440ce21dc6af829cc724f211d9fd309
gursewakinsan <insagursewakinsa@gmail.com>
2024-06-20 14:58:16 +0530

    changes

---
+++ Controls/CustomBusyIndicator.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WorqApp.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomBusyIndicator : StackLayout
+    {
+        public CustomBusyIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
